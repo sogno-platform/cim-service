8ba7c90063dd7f7494ff44c403fb367063f604bc
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-07-29 10:24:47 +0200

    Added auto-generated but modified model data structures

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from models.error import Error
+from models.model import Model
+from models.model_element import ModelElement
+from models.model_element_attributes import ModelElementAttributes
+from models.model_element_update import ModelElementUpdate
+from models.model_update import ModelUpdate
+from models.new_model import NewModel
+from models.new_model_element import NewModelElement
