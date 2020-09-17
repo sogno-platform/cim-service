52adbb55ff519345e5e3e0742efd28df920550b4
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-09-18 00:18:46 +0200

    Added generated files from openapi-generator

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,8 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
+from models.analysis import Analysis
+from models.analysis_response import AnalysisResponse
 from models.error import Error
 from models.model import Model
 from models.model_element import ModelElement
@@ -11,3 +13,7 @@ from models.model_element_update import ModelElementUpdate
 from models.model_update import ModelUpdate
 from models.new_model import NewModel
 from models.new_model_element import NewModelElement
+from models.outage_analysis import OutageAnalysis
+from models.outage_analysis_all_of import OutageAnalysisAllOf
+from models.powerflow_analysis import PowerflowAnalysis
+from models.powerflow_analysis_all_of import PowerflowAnalysisAllOf
