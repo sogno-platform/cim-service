fad72e4ccdb0a56f4dc9f5b02afe5f1abaa21d61
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-11-20 11:47:49 +0100

    Use redis for model db

--- Dockerfile
+++ Dockerfile
@@ -77,10 +77,10 @@ WORKDIR /usr/src/app
 COPY requirements-cimpy.txt /usr/src/app/
 RUN pip3 install --no-cache-dir -r requirements-cimpy.txt
 
+RUN pip3 install redis
+
 COPY . /usr/src/app
 
 EXPOSE 8080
 
-# ENTRYPOINT ["python3"]
-
-# CMD ["-m", "openapi_server"]
+ENTRYPOINT [ "/usr/bin/python3", "server.py"]
--- db.py
+++ db.py
@@ -6,7 +6,9 @@ import shelve
 from dataclasses import dataclass
 import random
 from os import urandom
+import redis
 random.seed(int.from_bytes(urandom(4), byteorder='big'))
+from tempfile import SpooledTemporaryFile
 
 @dataclass
 class record:
@@ -14,9 +16,10 @@ class record:
     cimobj: dict
     files: []
 
-
-# We do not use Writeback mode here, so beware
-models = shelve.open("cimpy.db")
+# The hostname "redis" is set for development as an alias
+# in the docker-compose.yaml file. For production this will
+# resolve via the kubernetes service name
+connection = redis.Redis("redis")
 
 def get_model(model_id):
     """Get a network model
@@ -27,25 +30,40 @@ def get_model(model_id):
 
     :rtype: Model
     """
-    global models
-    try:
-        return models[str(model_id)].model
-    except KeyError:
-        return None
-
+    model = connection.get(model_id).decode("utf-8")
+    cimpy_data = connection.get(str(model_id) + "_cimpy").decode('utf-8')
+    files_len = int(connection.get(str(model_id) + "_files_len").decode('utf-8'))
+    files = []
+    for index in range(files_len):
+        data_addr = str(model_id) + "_file_" + str(index)
+        data = connection.get(data_addr)
+        newfile = SpooledTemporaryFile()
+        newfile.write(data)
+        files.append(newfile)
+    return record( model, cimpy_data, files )
 
 def get_models():
     """Get a list of all network models
 
     :rtype: dict
     """
-    global models
-    if models == {}:
-        return Error(code=404, message="No models found in database"), 404
-    model_list = []
-    for key, rec in models.items():
-        model_list.append(ModelReply.from_model(rec.model, int(key)))
-    return model_list
+    def decode(a):
+        print(a)
+        model_id_char   = a.decode('utf-8')
+        model_bytes     = connection.get(model_id_char)
+        if model_bytes != None:
+            model       = eval(model_bytes.decode('utf-8'))
+        else:
+            model       = { "name"    : "NOT FOUND",
+                            "version" : "cgmes_v2_4_15",
+                            "profiles": [] }
+        model['id']     = int(model_id_char)
+        return model
+
+    if connection.smembers != None:
+        return list(map(decode, connection.smembers("models")))
+    else:
+        return []
 
 
 def put_model(new_model, cimpy_data, files):
@@ -55,16 +73,18 @@ def put_model(new_model, cimpy_data, files):
     :param cimpy_data: data extracted from files by cimpy
     :rtype: integer
     """
-    global models
-
-    # generate a new UUID which will be the model ID
-    new_id = random.getrandbits(32)
-    # Ensure ID is unique
-    while str(new_id) in models:
-        new_id = random.getrandbits(32)
-
-    models[str(new_id)] = record(new_model, cimpy_data, files)
-
+    # atomic increment the model_instance
+    # variable to get a unique id
+    new_id = connection.incr("model_instance")
+    connection.sadd("models", str(new_id))
+    connection.set(str(new_id), new_model.__repr__())
+    connection.set(str(new_id) + "_cimpy", cimpy_data.__repr__())
+    connection.set(str(new_id) + "_files_len", len(files))
+    for index,file_ in enumerate(files):
+        file_.seek(0)
+        data = file_.read()
+        data_addr = str(new_id) + "_file_" + str(index)
+        connection.set(data_addr, data)
     return new_id
 
 def delete_model(model_id):
@@ -75,11 +95,19 @@ def delete_model(model_id):
 
     :rtype: Model
     """
-    if str(model_id) in models:
-        model_reply = ModelReply.from_model(models[str(model_id)].model, model_id)
-        del models[str(model_id)]
-        return model_reply
+    model_bytes     = connection.get(model_id)
+    if model_bytes != None:
+        model       = eval(model_bytes.decode('utf-8'))
+        model['id'] = int(model_id)
     else:
         return Error(code=404, message="Cannot delete model with id: " + str(model_id) + ", not found in database"), 404
 
-
+    files_len_bytes = connection.get(str(model_id) + "_files_len")
+    files_len       = int(files_len_bytes.decode('utf-8'))
+    connection.delete(str(model_id))
+    connection.delete(str(model_id) + "_cimpy")
+    connection.delete(str(model_id) + "_files_len")
+    for index in range(files_len):
+        connection.delete(str(model_id) + "_file_" + str(index))
+    connection.srem("models", model_id)
+    return model
